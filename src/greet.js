4dd08c6a2110b4cc441eca74609ce7269126a939
Clemens Schueller <schueller.cj@gmail.com>
2021-05-19 08:59:58 +0200

    testing exercises and show

--- src/greet.js
+++
@@ -1,6 +0,0 @@
-export default function greet(...rest) {
-  if (rest.length === 0) return 'Hi, stranger'
-  if (rest.length > 1) return 'Hi, guys'
-  if (rest[0] === 'Jerry') return 'Hi, coach'
-  return `Hi, ${rest[0]}`
-}
--- src/math.js
+++
@@ -1,5 +0,0 @@
-export function add(...rest) {
-  //rest parameter ...rest
-  return rest.reduce((sum, cur) => sum + cur)
-  return sum + cur
-}
